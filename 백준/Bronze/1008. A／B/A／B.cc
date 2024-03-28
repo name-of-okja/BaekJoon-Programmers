9d4c0611d3128d5a13bc2a122fc83d6d5fa7dcf5
OkJa <151524504+name-of-okja@users.noreply.github.com>
2024-03-28 09:01:52 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a, b, result;
+    cin >> a >> b;
+    result = a / b;
+    cout.precision(10);
+    cout << result << endl;
+    return 0;
+}
