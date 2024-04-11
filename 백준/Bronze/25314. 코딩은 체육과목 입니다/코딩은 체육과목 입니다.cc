8161571a715a95e319dc3a0556e5b00b2f23bd1a
OkJa <151524504+name-of-okja@users.noreply.github.com>
2024-04-11 11:46:50 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    int n = a / 4;
+
+    while (n--)
+    {
+        cout << "long ";
+    }
+
+    cout << "int" << endl;
+}
