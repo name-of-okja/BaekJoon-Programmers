cb2c632b2a4db24e8a9c27b66cb700ce2cde11c6
OkJa <151524504+name-of-okja@users.noreply.github.com>
2024-04-15 09:35:01 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == 0 && b == 0)
+        {
+            return 0;
+        }
+
+        cout << a + b << endl;
+    }
+}
